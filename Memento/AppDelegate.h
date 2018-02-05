fb348ebc6bd263ba9d3d23ff8164dbb1fda98c5b
Yura Savchuk <yura.savchuk22@gmail.com>
2018-02-05 20:05:39 +0200

    Initial Commit

---
+++ Memento/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Memento
+//
+//  Created by Yura Savchuk on 05.02.18.
+//  Copyright © 2018 Coulcod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Memento/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Memento
+//
+//  Created by Yura Savchuk on 05.02.18.
+//  Copyright © 2018 Coulcod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
